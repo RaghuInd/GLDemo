4f79fb7da919d3a71b0b81e7b4e2479d141c5af5
rdgowda79 <rdgowda79@gmail.com>
2022-11-09 11:31:18 +0530

    Static demo added

---
+++ src/com/app/staticc/Demo.java
@@ -0,0 +1,25 @@
+package com.app.staticc;
+
+public class Demo {
+	public static Integer data = 100;// static data member
+	public Integer value = 200;// instance data member
+
+	public void display() { // instance method i.e available by creating new object
+		System.out.println("static data:" + data + " Value:" + value);
+		// display2();
+	}
+
+	public static void display2() {// belongs to class , no need of object of Demo
+		System.out.println("static data:" + data + " Value:?");// +value);
+		// display();
+	}
+
+	public Integer getData() {
+		return data;
+	}
+
+	public void setData(Integer data) {
+		Demo.data = data;
+	}
+
+}
---
+++ src/com/app/staticc/Driver.java
@@ -0,0 +1,25 @@
+package com.app.staticc;
+
+public class Driver {
+
+	 String name ="GL";
+	
+	public static void main(String[] args) {
+		System.out.println(Demo.data);
+		Demo demo = new Demo();
+		System.out.println(demo.value);
+		Demo.display2();
+		demo.display();
+		//name="hi";
+		//System.out.println(name);
+		Demo demo2 = new Demo();
+		demo.setData(99);
+		demo.display();
+		demo2.display();
+		demo2.setData(999);
+		demo.display();
+		
+
+	}
+
+}
